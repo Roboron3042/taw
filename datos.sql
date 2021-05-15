fd479a901427c9758d4b6e6360d9a57212a73885
Roboron3042 <robertoms258@gmail.com>
2021-05-15 19:12:39 +0200

    ADD: Initial data script

---
+++ datos.sql
@@ -0,0 +1,14 @@
+/**
+ * Author:  Rober
+ */
+
+INSERT INTO EVENTOS VALUES (0, 'Concierto de Mecano', 'Un concierto de Mecano para los más ochenteros', 20211212, 20211212, 50, 3000, 1, 'Asientos');
+
+INSERT INTO ASIENTOS VALUES (0, 30, 100, 3000, 0);
+
+INSERT INTO USUARIO VALUES ('admin@eventos.com', 'Administrador', 'De la Aplicación', 'Calle Eventos', 'Nº 42', 30, 'Desconocido', 'admin', 'admin');
+INSERT INTO USUARIO VALUES ('robmicsan@uma.es', 'Roberto', 'Michán Sánchez', 'Málaga', 'Teatinos', 24, 'Masculino', 'user', '12345678');
+
+INSERT INTO ETIQUETA VALUES (0, 'Concierto');
+INSERT INTO ETIQUETA VALUES (1, 'Teatro');
+INSERT INTO ETIQUETA VALUES (2, 'Manifestación');
